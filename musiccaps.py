af2465691e969dfe290ec5d4113fb1d62a1fcda5
Martin Karlsson <martin.karlsson@piktiv.se>
2024-04-24 11:23:33 +0200

    "complete download script"

--- musiccaps.py
+++ musiccaps.py
@@ -2,6 +2,7 @@ from pytube import YouTube, exceptions
 from moviepy.editor import *
 import pandas as pd
 import regex as re
+import json
 
 def timestamp_to_seconds(timestamp):
     [hours, minutes, seconds] = timestamp.split(":")
@@ -28,39 +29,73 @@ def download_video_by_id(youtube_id):
     output_path = stream.download('downloaded', filename=filename)
     return output_path
 
+def mp4_to_mp3(mp4_path):
+    output_path = mp4_path.replace("mp4", "mp3")
+    os.system(f"ffmpeg -i {mp4_path} {output_path}")
+    return output_path
+
 def cut_video(path_to_video, start, duration):
     output = path_to_video.replace("downloaded", "edited")
     start = seconds_to_timestamp(start)
     duration = seconds_to_timestamp(duration)
     command = f"ffmpeg -i {path_to_video} -ss {start} -t {duration} -c:a copy {output}"
     os.system(command=command)
-    converted_path = output.replace("mp4", "mp3")
-    os.system(f"ffmpeg -i {output} {converted_path}")
+    converted_video = mp4_to_mp3(output)
     os.remove(output)
     os.remove(path_to_video)
+    return converted_video
 
 def download_and_cut_video(youtube_id, start):
     print('Downloading...')
     input = download_video_by_id(youtube_id=youtube_id)
     print('Cutting...')
-    cut_video(input, start=start, duration=10)
+    edited_video = cut_video(input, start=start, duration=10)
     print('Done!')
+    return edited_video
 
-dataset = pd.read_csv("musiccaps-public.csv")
-dataset = dataset.loc[:, ['ytid', 'start_s']]
+def write_video_to_jsonl(jsonl_file_path, path):
+    line = '{"path": "' + path + '", "duration": 10, "sample_rate": 48000, "amplitude": null, "weight": null, "info_path": null}\n'
+    with open(jsonl_file_path, 'a', encoding='utf8') as outfile:
+        outfile.write(line)
+        outfile.close()
 
-number_of_videos_to_download = 100
-number_of_failed_downloads = 0
-for i in range(0,number_of_videos_to_download+1):
-    print(f"Downloading video {i}/{number_of_videos_to_download}")
-    try:
-        for path, directories, files in os.walk('C:/Users/marti/Documents/Exjobb/AudioLM/Musiccaps/edited'):
-            if dataset.loc[i, 'ytid']+'.mp3' not in files:
-                download_and_cut_video(dataset.loc[i, 'ytid'], dataset.loc[i, 'start_s'])
-            else:
-                print("Already downloaded!")
-    except (exceptions.AgeRestrictedError, exceptions.VideoUnavailable):
-        number_of_failed_downloads += 1
-        continue
+def write_json_meta_data(youtube_id, dataset):
+    template = {"key": "", "artist": "", "sample_rate": 48000, "file_extension": "mp3", "description": "", 
+                "keywords": "", "duration": 10.0, "bpm": "", "genre": "", "title": "Enracinement", 
+                "name": "", "instrument": "", "moods": []}    
+    row = dataset.loc[dataset['ytid'] == youtube_id]
+    print(row.loc[:,'caption'].iloc[0])
+    template["description"] = row.loc[:, 'caption'].iloc[0]
+    print(template["description"])
+    template["name"] = youtube_id
+    template["title"] = youtube_id
+    template["keywords"] = row.loc[:, 'aspect_list'].iloc[0]
+    out_file = open("edited/"+youtube_id + ".json", "w") 
+    json.dump(template, out_file, indent = 6) 
+    out_file.close() 
 
-print(f"Downloads complete! Successfully downloaded {number_of_videos_to_download-number_of_failed_downloads}/{number_of_videos_to_download}")
+    
+
+def main():
+    dataset = pd.read_csv("musiccaps-public.csv")
+    number_of_videos_to_download = 100
+    number_of_failed_downloads = 0
+    for i in range(0,number_of_videos_to_download+1):
+        print(f"Downloading video {i}/{number_of_videos_to_download}")
+        try:
+            for path, directories, files in os.walk('./edited'):
+                if dataset.loc[i, 'ytid']+'.mp3' not in files:
+                    video = download_and_cut_video(dataset.loc[i, 'ytid'], dataset.loc[i, 'start_s'])
+                    write_video_to_jsonl('data.jsonl', video)
+                    write_json_meta_data(dataset.loc[i, 'ytid'], dataset)
+                else:
+                    print("Already downloaded!")
+        except (exceptions.AgeRestrictedError, exceptions.VideoUnavailable):
+            number_of_failed_downloads += 1
+            continue
+
+    print(f"Downloads complete! Successfully downloaded {number_of_videos_to_download-number_of_failed_downloads}/{number_of_videos_to_download}")
+
+
+if __name__ == "__main__":
+    main()
